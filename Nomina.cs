5032c6168efbb63732251d72cd558dc0b9d5aeed
Juandavidome <juandavidome320@gmail.com>
2022-06-13 17:37:07 -0500

    F

---
+++ Nomina.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomina01
+{
+    class PayRoll:Program
+    {
+        private double NumberDocument;
+        private string FirtsName;
+        private string LastName;
+        private double Salary;
+        private double WorkedDays;
+
+        public PayRoll()
+        {
+        }
+        public PayRoll(string firtsName, string lastName)
+        {
+            FirtsName = firtsName;
+            LastName = lastName;
+        }
+
+        public PayRoll(double numberDocument, double salary, double workedDays)
+        {
+            NumberDocument = numberDocument;
+            Salary = salary;
+            WorkedDays = workedDays;
+        }
+
+        public void setnumberdocument(double numberDocument)
+        {
+            this.NumberDocument = numberDocument;
+        }
+        public double getnumberDocument()
+        {
+            return NumberDocument;
+        }
+        public void setFirst(string firstname)
+        {
+            this.FirtsName = firstname;
+        }
+        public string getFirst()
+        {
+            return FirtsName;
+        }
+        public void setlast(string lastName)
+        {
+            this.LastName = lastName;
+        }
+        public string getlast()
+        {
+            return LastName;
+        }
+        public void setsalary(double salary)
+        {
+            this.Salary = salary;
+        }
+        public double getsalary()
+        {
+            return Salary;
+        }
+        public void setworked(double workedDays)
+        {
+            this.WorkedDays= workedDays;
+        }
+        public double getworked()
+        {
+            return WorkedDays;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomina01
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            PayRoll MyPayRoll = new PayRoll();
+            Console.WriteLine("1.Ingrese su numero de Documento:");
+            MyPayRoll.setnumberdocument(double.Parse(Console.ReadLine()));
+            Console.WriteLine("\n2.Ingrese su primer nombre: ");
+            MyPayRoll.setFirst((Console.ReadLine()));
+            Console.WriteLine("\n3.Ingrese su apellido: ");
+            MyPayRoll.setlast((Console.ReadLine()));
+            Console.WriteLine("\n4.Ingrese su salario: ");
+            MyPayRoll.setsalary(double.Parse(Console.ReadLine()));
+            Console.WriteLine("\n5.Ingrese los dias que trabaja: ");
+            MyPayRoll.setworked(double.Parse(Console.ReadLine()));
+
+
+
+
+
+            
+
+
+        }
+
+    }
+}
